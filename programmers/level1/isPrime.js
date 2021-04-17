5f748839e0b18a5332cd31bd358fb0cd1f6a42d9
DohyungKim <ehgud456456@naver.com>
2021-04-17 19:34:11 +0900

    ps

---
+++ programmers/level1/isPrime.js
@@ -0,0 +1,38 @@
+function solution(n) {
+    let count = 0;
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) count++
+    }
+    return count;
+}
+
+function isPrime(n) {
+    if (n == 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    let root = Math.floor(Math.sqrt(n));
+    for (let i = 3; i <= root; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+
+function solution(n) {
+    let answer = [];
+    let count = 0;
+    for (let i = 2; i <= n; i++) {
+        answer[i] = i;
+    }
+    for (let i = 2; i <= n; i++) {
+        if (answer[i] == 0) continue;
+        for (let j = 2 * i; j <= n; j = j + i) {
+            answer[j] = 0;
+        }
+    }
+    for (let i = 2; i <= n; i++) {
+        if (answer[i] != 0) count++;
+    }
+
+    return count;
+}
