0c24fb981192a2428d4c9dbd695a163aeed56050
DohyungKim <ehgud456456@naver.com>
2022-03-01 20:19:19 +0900

    Update two.js

--- kakao/kakao_second/kakao_second_2021/two.js
+++ kakao/kakao_second/kakao_second_2021/two.js
@@ -1,28 +1,26 @@
-let {auth_key: twoKey, time: twoTime} = await START(2);
-
-// console.log(twoKey, `twoTime);
+let { auth_key: twoKey, time: twoTime } = await START(2);
 
 while (twoTime < 720) {
 	let {
-		data: {locations},
+		data: { locations },
 	} = await getLocations(twoKey);
 	// console.log(locations);
 
 	let {
-		data: {trucks},
+		data: { trucks },
 	} = await getTrucks(twoKey);
 	// console.log(trucks);
 
 	let commands = Command(locations, trucks, 2);
 	// console.log(commands);
 
-	let {data: simulData} = await simulateCommand(twoKey, commands);
+	let { data: simulData } = await simulateCommand(twoKey, commands);
 	// console.log(simulData);
 	twoTime = simulData.time;
 }
 
 const {
-	data: {score: scoreTwo},
+	data: { score: scoreTwo },
 } = await getScore(twoKey);
 
 // console.log(scoreTwo);
