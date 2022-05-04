7280ef7b9e270d386d87b53d0bb04c6d1fda9303
DohyungKim <ehgud456456@naver.com>
2022-05-04 22:34:34 +0900

    ps 0429

---
+++ leetcode/Algorithm/77Combinations.ts
@@ -0,0 +1,17 @@
+// * 2057 0743
+
+function combine(n: number, k: number): number[][] {
+	let result = [];
+
+	for (let i = 1; i <= n - k + 1; i++) {
+		DFS([i], k);
+	}
+	function DFS(arr, num) {
+		if (num === 1) result.push(arr);
+		for (let i = arr[arr.length - 1] + 1; i <= n; i++) {
+			DFS([...arr, i], num - 1);
+		}
+	}
+
+	return result;
+}
