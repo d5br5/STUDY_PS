5c6b769307da03e1de73463224f80ceda7ba3655
Doh Kim <ehgud456456@naver.com>
2022-05-10 10:07:47 +0900

    ps 0510

--- leetcode/Algorithm/191Numberof1Bits.js
+++ leetcode/Algorithm/191Numberof1Bits.js
@@ -11,3 +11,30 @@ var hammingWeight = function (n) {
 	}
 	return count + 1;
 };
+
+/** 7886 9465
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+	let count = 0;
+	let mask = 1;
+	for (let i = 0; i < 32; i++) {
+		if ((n & mask) != 0) count++;
+		mask <<= 1;
+	}
+	return count;
+};
+
+/** 5246 9990
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+	let count = 0;
+	while (n != 0) {
+		count++;
+		n &= n - 1;
+	}
+	return count;
+};
