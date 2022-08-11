527b3219394e113542b46be1d18c37ae31edd26a
Doh Kim <ehgud456456@naver.com>
2022-08-11 10:46:12 +0900

    ps 0811

--- leetcode/now/11ContainerWithMostWater.js
+++ leetcode/now/11ContainerWithMostWater.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var maxArea = function (height) {
+	let max = 0;
 	let left = 0;
 	let right = height.length - 1;
-	let max = 0;
+
 	while (left < right) {
 		let area = Math.min(height[left], height[right]) * (right - left);
 		if (max < area) max = area;
