750650c0990150530b2d5e9336a65304b1c4c2c6
Doh Kim <ehgud456456@naver.com>
2022-10-04 10:19:14 +0900

    ps 1003

--- leetcode/now/232ImplementQueueUsingStacks.js
+++ leetcode/now/232ImplementQueueUsingStacks.js
@@ -1,4 +1,4 @@
-// 6112 2076
+// 5132 9353
 
 var MyQueue = function () {
 	this.stack = [];
@@ -41,4 +41,55 @@ MyQueue.prototype.empty = function () {
  * var param_4 = obj.empty()
  * var param_3 = obj.peek()
 
+ */
+
+// 8658 4406
+
+var MyQueue = function () {
+	this.stack = [];
+	this.stack2 = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+	while (this.stack2.length > 0) {
+		this.stack.push(this.stack2.pop());
+	}
+	this.stack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+	while (this.stack.length > 0) {
+		this.stack2.push(this.stack.pop());
+	}
+	return this.stack2.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+	return this.stack[0] || this.stack2[this.stack2.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+	return this.stack.length === 0 && this.stack2.length === 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
  */
