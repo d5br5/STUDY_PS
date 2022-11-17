4e00aff8fbf2765445cf6332fdaacb21cad4a2c1
Doh Kim <ehgud456456@naver.com>
2022-11-17 15:32:44 +0900

    ps 1117

---
+++ leetcode/now/71SimplifyPath.js
@@ -0,0 +1,16 @@
+/** 6116 9644
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+	let stack = [];
+	for (let p of path.split("/")) {
+		if (p === "" || p === ".") continue;
+		if (p === "..") {
+			stack.pop();
+		} else {
+			stack.push(p);
+		}
+	}
+	return "/" + stack.join("/");
+};
